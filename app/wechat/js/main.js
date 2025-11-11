dfc5149e9a7fb6f1bcadc57a738301f1bd9ba124
luckyz742 <2687300341@qq.com>
2025-11-12 04:12:10 +0800

    Create main.js

---
+++ app/wechat/js/main.js
@@ -0,0 +1,19 @@
+// 更换头像按钮的功能
+document.querySelector('.change-avatar-btn').addEventListener('click', function() {
+  alert('头像已更换！');
+});
+
+// 摇一摇按钮的功能
+document.querySelector('.shake-btn').addEventListener('click', function() {
+  alert('你摇了摇手机！');
+});
+
+// 联系人按钮功能
+document.querySelector('.contacts-btn').addEventListener('click', function() {
+  alert('打开联系人列表！');
+});
+
+// 朋友圈按钮功能
+document.querySelector('.moments-btn').addEventListener('click', function() {
+  alert('查看朋友圈！');
+});
